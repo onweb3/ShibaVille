11a7e518115e6b2ebaa5a1a4c619619645cce3f0
ZainGTR <askaliceai@gmail.com>
2024-07-31 21:00:59 +0400

    Adding camera setup

--- client/src/main.js
+++ client/src/main.js
@@ -10,8 +10,28 @@ window.onload = () => {
     1000
   );
 
-  camera.position.set(0, 0, 100);
-  camera.lookAt(0, 0, 0);
+  let bIsMouseIsDown = false;
+  let prevMouseX = 0;
+  let prevMouseY = 0;
+  let cameraAzimuth = 0;
+  let cameraRadius = 4;
+  let cameraElevation = 0;
+  const DEG2RAD = Math.PI / 360;
+  updateCamera();
+
+  function updateCamera() {
+    camera.position.x =
+      cameraRadius *
+      Math.sin(cameraAzimuth * DEG2RAD) *
+      Math.cos(cameraElevation * DEG2RAD);
+    camera.position.y = cameraRadius * Math.sin(cameraElevation * DEG2RAD);
+    camera.position.z =
+      cameraRadius *
+      Math.cos(cameraAzimuth * DEG2RAD) *
+      Math.cos(cameraElevation * DEG2RAD);
+    camera.lookAt(0, 0, 0);
+    camera.updateMatrix();
+  }
 
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
@@ -24,24 +44,32 @@ window.onload = () => {
 
   scene.add(cube);
 
-  camera.position.z = 5;
-
   function gameLoop() {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
 
     renderer.render(scene, camera);
   }
 
   // mouse inputs
   function onMouseDown() {
-    console.log("Mouse Down event");
+    bIsMouseIsDown = true;
   }
   function onMouseUp() {
-    console.log("Mouse Up event");
+    bIsMouseIsDown = false;
   }
-  function onMouseMove() {
-    console.log("Mouse Move event");
+  function onMouseMove(e) {
+    console.log([e.screenX, e.screenY]);
+
+    if (bIsMouseIsDown) {
+      cameraAzimuth += (e.screenX - prevMouseX) * 0.5;
+      cameraElevation += (e.screenY - prevMouseY) * 0.5;
+      cameraElevation = Math.min(90, Math.max(0, cameraElevation));
+      updateCamera();
+    }
+
+    prevMouseX = e.screenX;
+    prevMouseY = e.screenY;
   }
 
   addEventListener("mousedown", onMouseDown, false);

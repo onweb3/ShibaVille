b659d791f1fc7f34aabb22c374f2bb094a88d32f
ZainGTR <askaliceai@gmail.com>
2024-08-12 06:16:33 +0400

    version update

--- contracts/Buildings.sol
+++ contracts/Buildings.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.26;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
--- contracts/ShibaVille.sol
+++ contracts/ShibaVille.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.26;
 import "./Ville.sol";
 import "./Buildings.sol";
 import "./BuildingInfo.sol";

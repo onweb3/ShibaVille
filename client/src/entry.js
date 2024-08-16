3cf527c7ec5b6565f128298c4e7c681fbcda4c6e
ZainGTR <askaliceai@gmail.com>
2024-08-16 12:05:18 +0400

    removing mm sdk, using window ethereum from now on.

--- client/src/entry.js
+++ client/src/entry.js
@@ -1,12 +1,89 @@
-import { metamaskAuth } from "./auth.js";
 import { createScene } from "./main.js";
 import { createVille } from "./ville.js";
 
-function createGame() {
-  // const mmSdk = metamaskAuth();
-  // if (!mmSdk.connectedAccount) {
-  //   mmSdk.connect();
-  // }
+async function createGame() {
+  let currentAccount;
+
+  const connectBtn = document.getElementById("metamask-btn");
+  const chainBtn = document.getElementById("chain-state");
+  if (window.ethereum && window.ethereum.isMetaMask) {
+    window.ethereum.on("accountsChanged", handleAccountsChanged);
+    window.ethereum.on("chainChanged", handleChainChanged);
+    window.ethereum.on("connect", checkChainId);
+    connectBtn.addEventListener("click", connect);
+    chainBtn.addEventListener("click", changeChain);
+    checkConnection();
+    checkChainId();
+  } else {
+    connectBtn.innerText = "Please install MetaMask!";
+  }
+
+  function connect() {
+    window.ethereum
+      .request({ method: "eth_requestAccounts" })
+      .then(handleAccountsChanged)
+      .catch((err) => {
+        if (err.code === 4001) {
+          console.log("Please connect to MetaMask.");
+        } else {
+          console.error(err);
+        }
+      });
+  }
+
+  function checkConnection() {
+    window.ethereum
+      .request({ method: "eth_accounts" })
+      .then(handleAccountsChanged)
+      .catch(console.error);
+  }
+
+  function checkChainId() {
+    window.ethereum
+      .request({ method: "eth_chainId" })
+      .then(handleChainChanged)
+      .catch(console.error);
+  }
+
+  function changeChain() {
+    window.ethereum
+      .request({
+        method: "wallet_addEthereumChain",
+        params: [
+          {
+            chainId: "0x15eb",
+            chainName: "opBNB",
+            blockExplorerUrls: ["https://testnet.opbnbscan.com"],
+            nativeCurrency: { symbol: "tBNB", decimals: 18 },
+            rpcUrls: ["https://opbnb-testnet-rpc.bnbchain.org"],
+          },
+        ],
+      })
+      .then((res) => console.log("add", res))
+      .catch((e) => console.log("ADD ERR", e));
+  }
+
+  function handleChainChanged(chainId) {
+    console.log(chainId);
+
+    if (chainId !== "0x15eb") {
+      chainBtn.innerText = "Wrong Network!";
+      chainBtn.disabled = false;
+    } else {
+      chainBtn.innerText = "OPBNB";
+      chainBtn.disabled = true;
+    }
+  }
+  function handleAccountsChanged(accounts) {
+    console.log(accounts);
+
+    if (accounts.length === 0) {
+      connectBtn.innerText = "Connect to MetaMask";
+    } else if (accounts[0] !== currentAccount) {
+      currentAccount = accounts[0];
+      connectBtn.innerText = `Address: ${currentAccount}`;
+    }
+  }
 
   const scene = createScene(window);
   const ville = createVille();
